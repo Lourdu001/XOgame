f65da0ff176716783a52b99b8320e762e23d8fd8
Lourdu001 <rajlourdhu889@gmail.com>
2025-07-02 12:54:52 +0530

    vite.config

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,8 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
+// https://vitejs.dev/config/
 export default defineConfig({
+  base: '/XOgame/',  // 👈 Add this line (repo name with slashes)
   plugins: [react()],
 })
